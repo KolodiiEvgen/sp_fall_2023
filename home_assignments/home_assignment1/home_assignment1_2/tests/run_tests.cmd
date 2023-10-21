884ec51fe00706e7b50eae943ca7c7c2bbb463ad
KolodiiEvgen <148628813+KolodiiEvgen@users.noreply.github.com>
2023-10-21 23:32:00 +0200

    Update run_tests.cmd

--- home_assignments/home_assignment1/home_assignment1_2/tests/run_tests.cmd
+++ home_assignments/home_assignment1/home_assignment1_2/tests/run_tests.cmd
@@ -1,3 +1,27 @@
-CALL "%~dp0..\..\..\run_tests.cmd" "%~dp0" "home_assignment1_2"
+#include <stdio.h>
 
-EXIT /B %ERRORLEVEL%
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N <= 0 || N >= 100) {
+        printf("Недопустимое значение N\n");
+        return 1;
+    }
+
+    int sum = 0;
+    int num;
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &num);
+        if (num > 0) {
+            sum += num;
+        } else {
+            printf("Введено отрицательное число, программа игнорирует его.\n");
+        }
+    }
+
+    float average = (float)sum / N;
+    printf("%f\n", average);
+
+    return 0;
+}
